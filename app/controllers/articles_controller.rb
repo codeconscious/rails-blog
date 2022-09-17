e781d9c4d98cdb6c5bc4a1e4e3549e8a0fbb3f41
CodeConscious <hello@codeconscio.us>
2022-09-17 22:34:43 +0900

    Article CRUD operations working

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,60 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @prev_id = Article.where('id < ?', @article.id)&.last&.id
+    @next_id = Article.where('id > ?', @article.id)&.first&.id
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+
+  def next(current_id)
+
+  end
+
+  def prev(current_id)
+    Article.where('id < ?', current_id).last
+  end
+end
