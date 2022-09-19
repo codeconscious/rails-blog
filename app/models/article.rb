e14e29a9398824397e4bc19399c3ed24f1a68cce
CodeConscious <hello@codeconscio.us>
2022-09-19 16:22:27 +0900

    Add article model scope tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ApplicationRecord
 
   has_many :comments, dependent: :destroy
 
+  scope :public_articles, -> { where(status: 'public') }
+  scope :private_articles, -> { where(status: 'private') }
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
