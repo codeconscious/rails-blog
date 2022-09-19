2bad18000e1715582f82c91c0e6a9ec5dd5971cd
CodeConscious <hello@codeconscio.us>
2022-09-19 17:50:46 +0900

    Add articles controller show test

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe ArticlesController, type: :controller do # TODO: Research :reques
       expect(response).to be_successful # Was be_success until Rails 6.
     end
   end
+
+  context 'GET /show' do
+    let (:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
+    before(:each) do
+      Article.new(params).save
+      Article.new(params).save
+    end
+
+    it 'returns a successful response' do
+      get :show, params: { id: Article.first.to_param }
+      expect(response).to be_successful
+    end
+  end
 end
