737af7ecdc2fc1c22b19b9586f9feb89d2ca5663
CodeConscious <hello@codeconscio.us>
2022-09-25 21:49:28 +0900

    Add articles controller /create test

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ArticlesController, type: :controller do # TODO: Research :reques
   end
 
   context 'GET /show' do
-    let (:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
+    let(:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
     before(:each) do
       Article.new(params).save
       Article.new(params).save
@@ -21,6 +21,16 @@ RSpec.describe ArticlesController, type: :controller do # TODO: Research :reques
     end
   end
 
+  context 'POST /create' do
+    let(:valid_params) { { 'article' => { 'title' => 'Article Title', 'body' => 'Body of article.', 'status' => 'public' } } }
+
+    it 'creates new article given valid parameters' do
+      expect do
+        post :create, params: valid_params
+      end.to change(Article, :count).by(1)
+    end
+  end
+
   context 'GET /new' do
     it 'returns a successful response' do
       get :new
@@ -29,7 +39,7 @@ RSpec.describe ArticlesController, type: :controller do # TODO: Research :reques
   end
 
   context 'GET /edit' do
-    let (:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
+    let(:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
     before(:each) do
       Article.new(params).save
     end
@@ -40,13 +50,13 @@ RSpec.describe ArticlesController, type: :controller do # TODO: Research :reques
     end
   end
 
-  context 'GET /destroy' do
-    let (:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
+  context 'DELETE /destroy' do
+    let(:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
     before(:each) do
       Article.new(params).save
     end
 
-    it 'reduces article count by 1 successfully (article ID passed)' do
+    it 'deletes specified article, reducing article count by 1 (article ID passed)' do
       expect do
         delete :destroy, params: { id: Article.first.to_param }
       end.to change(Article, :count).by(-1)
