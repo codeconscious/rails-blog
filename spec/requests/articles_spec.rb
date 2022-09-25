c1671f419b2dc1ca5884840c03e7555298f3fb78
CodeConscious <hello@codeconscio.us>
2022-09-25 21:09:18 +0900

    Add articles controller tests

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -20,4 +20,36 @@ RSpec.describe ArticlesController, type: :controller do # TODO: Research :reques
       expect(response).to be_successful
     end
   end
+
+  context 'GET /new' do
+    it 'returns a successful response' do
+      get :new
+      expect(response).to be_successful
+    end
+  end
+
+  context 'GET /edit' do
+    let (:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
+    before(:each) do
+      Article.new(params).save
+    end
+
+    it 'returns a successful response (article ID passed)' do
+      get :edit, params: { id: Article.first.to_param }
+      expect(response).to be_successful
+    end
+  end
+
+  context 'GET /destroy' do
+    let (:params) { { title: 'Test Title', body: 'Test body.', status: 'public' } }
+    before(:each) do
+      Article.new(params).save
+    end
+
+    it 'reduces article count by 1 successfully (article ID passed)' do
+      expect do
+        delete :destroy, params: { id: Article.first.to_param }
+      end.to change(Article, :count).by(-1)
+    end
+  end
 end
